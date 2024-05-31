e6be629bad1d6627b2344eb87bf576b3ca3c4768
Rajan <rajanpatel2684@gmail.com>
2024-05-31 22:35:52 +0530

    Latest Resume updated

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,7 +13,7 @@ function About() {
                         <div id='about_me_title'>ABOUT ME</div>
                         <div id='about_text'>Hello, esteemed visitor. Welcome to my website. I am Rajan Patel, having completed my Bachelor's in Technology with a major in Computer Science and Engineering at Sardar Vallabhbhai National Institute of Technology, Surat. Previously, I undertook an internship at the John Deere Manufacturing department as a Graduate Engineering Trainee during the summer break at the end of my third year, in Pune, Maharashtra. I graduated with my Bachelor's in Technology in June 2024.</div>
                         <span>
-                            <a href="https://drive.google.com/file/d/1jBrnXWH36kHf7JUtjnE5z0bnV1JbhGsQ/view" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                            <a href="https://drive.google.com/file/d/1R7B56hkfdTHY4N2IPFKHN1sQmvnAkpDJ/view?usp=drive_link" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
                                 <div id='button'>Download CV <GrDocumentDownload style={{ paddingLeft: "6px", fontSize:"26px" }} /></div>
                             </a>
                         </span>
